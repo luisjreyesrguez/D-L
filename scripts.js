08ee8876d4fcdf7c582962dfc9b698be73a09e9b
Luis Reyes <luis.reyes@g-tech.io>
2024-09-17 13:18:44 -0600

    v1

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+document.getElementById('celebrate-button-1').addEventListener('click', function() {
+    confetti({
+        particleCount: 200,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+});
+
+document.getElementById('celebrate-button-2').addEventListener('click', function() {
+    confetti({
+        particleCount: 200,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+});
+
